b4da8223aead0109013b0afeb17aae8014f2c4c7
hashem <74487162+hashemtb@users.noreply.github.com>
2022-10-05 13:56:42 +0300

    Create

--- analysis/enterData/src/main/java/com/atypon/enterData/model/TemperatureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atypon.enterData.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TemperatureRepository extends CrudRepository<Temperature, Long> {
-
-}
